01251127ca7163d552f007ff578f876874f2bed1
Chris Darren Imanuel <chrisdarren05@gmail.com>
2025-09-17 15:01:31 +0700

    Tutorial 3 selesai

--- main/models.py
+++ main/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-# Create your models here.
 import uuid
 from django.db import models
 
+from django.contrib.auth.models import User
+
 class News(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     CATEGORY_CHOICES = [
         ('transfer', 'Transfer'),
         ('update', 'Update'),
--- main/views.py
+++ main/views.py
@@ -4,15 +4,33 @@ from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
 from main.models import News
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+@login_required(login_url='/login')
 def show_main(request):
-    news_list = News.objects.all()
+    filter_type = request.GET.get("filter", "all")
+
+    if filter_type == "all":
+        news_list = News.objects.all()
+    else:
+        news_list = News.objects.filter(user=request.user)
 
     context = {
         'npm' : '2406396956',
-        'name': 'Chris Darren Imanuel',
+        'name': request.user.username,
         'class': 'PBP D',
-        'news_list': news_list
+        'news_list': news_list,
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
 
     return render(request, "main.html", context)
@@ -21,12 +39,18 @@ def create_news(request):
     form = NewsForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        news_entry = form.save(commit=False)
+        news_entry.user = request.user
+        news_entry.save()
         return redirect('main:show_main')
 
-    context = {'form': form}
+    context = {
+        'form': form
+        }
+    
     return render(request, "create_news.html", context)
 
+@login_required(login_url='/login')
 def show_news(request, id):
     news = get_object_or_404(News, pk=id)
     news.increment_views()
@@ -37,6 +61,7 @@ def show_news(request, id):
 
     return render(request, "news_detail.html", context)
 
+# Tutor 2: display XML and JSON
 def show_xml(request):
     news_list = News.objects.all()
     xml_data = serializers.serialize("xml", news_list)
@@ -65,3 +90,38 @@ def show_json_by_id(request, news_id):
     
     except News.DoesNotExist:
         return HttpResponse(status=404)
+    
+# Tutor 3: user registration
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse("main:login"))
+    response.delete_cookie('last_login')
+    return response
